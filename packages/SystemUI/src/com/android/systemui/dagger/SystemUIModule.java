abe2b6c7fe28af1307139aab142ca2599943a414
Keith Mok <kmok@cyngn.com>
2020-11-10 10:47:09 -0300

    SystemUI: Add caffeine QS tile
    Add long press to infinity to caffeine qs tile
    
    Updated for android 12
    
    Co-authored-by: Adin Kwok <adin@adin.dev>
    Change-Id: If54ea27ddf179c2de39b61850163e14173727698
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -62,6 +62,7 @@ import com.android.systemui.keyboard.KeyboardModule;
 import com.android.systemui.keyevent.data.repository.KeyEventRepositoryModule;
 import com.android.systemui.keyguard.ui.view.layout.blueprints.KeyguardBlueprintModule;
 import com.android.systemui.keyguard.ui.view.layout.sections.KeyguardSectionsModule;
+import com.android.systemui.custom.CustomModule;
 import com.android.systemui.log.dagger.LogModule;
 import com.android.systemui.log.dagger.MonitorLog;
 import com.android.systemui.log.table.TableLogBuffer;
@@ -197,6 +198,7 @@ import javax.inject.Named;
         KeyboardModule.class,
         KeyguardBlueprintModule.class,
         KeyguardSectionsModule.class,
+        CustomModule.class,
         LetterboxModule.class,
         LogModule.class,
         MediaProjectionModule.class,
