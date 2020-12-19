48409e3c47310aff4b5892d26b9ad90b3901d5a4
IacobIonut01 <paulionut2003@gmail.com>
2020-12-19 15:23:09 +0000

    VolumeUI : Redesign Volume Panel Interface
    Based on DotOS 5.0's new guidelines
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: If08e357bbae9e2d71dc85532e284a3c966bb2bc0
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -264,7 +264,7 @@ public class VolumeDialogImpl implements VolumeDialog,
         mDialog = LayoutInflater.from(mContext).inflate(R.layout.volume_dialog,
                 (ViewGroup) null, false);
 
-        mDialog.setOnTouchListener((v, event) -> {
+       mDialog.setOnTouchListener((v, event) -> {
             if (mShowing) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_OUTSIDE:
@@ -331,6 +331,11 @@ public class VolumeDialogImpl implements VolumeDialog,
         if (mExpandRows != null) {
             setLayoutGravity(mExpandRows.getLayoutParams(), panelGravity);
             mExpandRows.setRotation(mVolumePanelOnLeft ? -90 : 90);
+        }
+
+        LinearLayout belowContainer = mDialog.findViewById(R.id.main_below);
+        if (belowContainer != null) {
+            setLayoutGravity(belowContainer.getLayoutParams(), panelGravity);
         }
 
         if (mRows.isEmpty()) {
@@ -525,11 +530,8 @@ public class VolumeDialogImpl implements VolumeDialog,
         row.view = LayoutInflater.from(mContext).inflate(R.layout.volume_dialog_row, null);
         row.view.setId(row.stream);
         row.view.setTag(row);
-        row.header = row.view.findViewById(R.id.volume_row_header);
+        row.header = row.view.findViewById(R.id.volume_row_percentage);
         row.header.setId(20 * row.stream);
-        if (stream == STREAM_ACCESSIBILITY) {
-            row.header.setFilters(new InputFilter[] {new InputFilter.LengthFilter(13)});
-        }
         row.dndIcon = row.view.findViewById(R.id.dnd_icon);
         row.slider = row.view.findViewById(R.id.volume_row_slider);
         row.slider.setOnSeekBarChangeListener(new VolumeSeekBarChangeListener(row));
@@ -1273,11 +1275,6 @@ public class VolumeDialogImpl implements VolumeDialog,
             row.slider.setMin(min);
         }
 
-        // update header text
-        Util.setText(row.header, getStreamLabelH(ss));
-        row.slider.setContentDescription(row.header.getText());
-        mConfigurableTexts.add(row.header, ss.name);
-
         // update icon
         final boolean iconEnabled = (mAutomute || ss.muteSupported) && !zenMuted;
         row.icon.setEnabled(iconEnabled);
@@ -1361,17 +1358,14 @@ public class VolumeDialogImpl implements VolumeDialog,
         boolean useActiveColoring = isActive && row.slider.isEnabled();
         final ColorStateList tint = useActiveColoring
                 ? Utils.getColorAccent(mContext)
-                : Utils.getColorAttr(mContext, android.R.attr.colorForeground);
+                : Utils.getColorAttr(mContext, android.R.attr.textColorSecondary);
         final int alpha = useActiveColoring
                 ? Color.alpha(tint.getDefaultColor())
                 : getAlphaAttr(android.R.attr.secondaryContentAlpha);
         if (tint == row.cachedTint && mExpanded) return;
         row.slider.setProgressTintList(tint);
-        row.slider.setThumbTintList(tint);
-        row.slider.setProgressBackgroundTintList(tint);
-        row.slider.setAlpha(((float) alpha) / 255);
         row.icon.setImageTintList(tint);
-        row.icon.setImageAlpha(alpha);
+        row.header.setTextColor(tint);
         row.cachedTint = tint;
     }
 
@@ -1425,6 +1419,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                 row.slider.setProgress(newProgress, true);
             }
         }
+        Util.setText(row.header, Utils.formatPercentage((enable && !row.ss.muted) ? vlevel : 0, row.ss.levelMax));
     }
 
     private void recheckH(VolumeRow row) {
@@ -1624,6 +1619,12 @@ public class VolumeDialogImpl implements VolumeDialog,
                 }
             }
             final int userLevel = getImpliedLevel(seekBar, progress);
+            if (mRow.ss.level == (mRow.ss.levelMin + 1) && userLevel <= mRow.ss.level) {
+                seekBar.setProgress((mRow.ss.levelMin + 1) * 100);
+                Util.setText(mRow.header,Utils.formatPercentage(mRow.ss.levelMin + 1, mRow.ss.levelMax));
+                return;
+            }
+            Util.setText(mRow.header, Utils.formatPercentage(userLevel, mRow.ss.levelMax));
             if (mRow.ss.level != userLevel || mRow.ss.muted && userLevel > 0) {
                 mRow.userAttempt = SystemClock.uptimeMillis();
                 if (mRow.requestedLevel != userLevel) {
