502ff0b66e07b0de709de6d9e3c5070a79cb6158
TikkiTikki <radstylemusic@gmail.com>
2021-05-02 18:04:37 -0300

    Add Android S Clocks
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -172,6 +172,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEJavaClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeAltClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new AndroidSClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new AndroidSDP3ClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
