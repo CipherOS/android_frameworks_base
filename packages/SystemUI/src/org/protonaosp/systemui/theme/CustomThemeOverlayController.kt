cc4b2c4ae9708c195457858f7a63dd8c108f3134
Sarthak Roy <sarthakroy2002@gmail.com>
2021-12-12 08:11:50 +0000

    Revert "SystemUI: theme: Log generated colors"
    * spams logs like hell
    
    This reverts commit af8e814a3c74d59170fa879d6e7d0524f399d2a8.
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- packages/SystemUI/src/org/protonaosp/systemui/theme/CustomThemeOverlayController.kt
+++ packages/SystemUI/src/org/protonaosp/systemui/theme/CustomThemeOverlayController.kt
@@ -24,7 +24,6 @@ import android.content.om.FabricatedOverlay
 import android.os.Handler
 import android.os.UserManager
 import android.provider.Settings
-import android.util.Log
 import android.util.TypedValue
 import com.android.systemui.broadcast.BroadcastDispatcher
 import com.android.systemui.dagger.SysUISingleton
@@ -143,7 +142,6 @@ class CustomThemeOverlayController @Inject constructor(
 
                 listEntry.value.forEach { (shade, color) ->
                     val colorSrgb = color.convert<Srgb>()
-                    Log.d(TAG, "Color $group $shade = ${colorSrgb.toHex()}")
                     setColor("system_${group}_$shade", colorSrgb)
                 }
             }
