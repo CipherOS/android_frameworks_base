60506cc78f790e4238262a23eb93ed45402f1b12
maxwen <max.weninger@gmail.com>
2022-01-02 23:34:05 +0100

    SystemUI: Allow forcing small keyguard clock [1/2]
    by default lockscreen clock will switch between big and
    small depending if notifications are shown or not
    
    but I think the small left aligned lock looks better
    especially on large screens so allow force using it
    
    idoybh (YAAP): Adapted, Improved weird logic that also affects other stuff outside of this function and could definitively cause unwanted behavior. Also actually toggle on runtime and initially, don't wait for a notification to show to actually take effect.
    
    Change-Id: I10646527ebff6d331d5b68e0adb5022a6e0d1324
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5179,6 +5179,13 @@ public final class Settings {
         public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
 
         /**
+         * Whether to force small clock on keyguard
+         * @hide
+         */
+        @Readable
+        public static final String LOCKSCREEN_SMALL_CLOCK = "lockscreen_small_clock";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
