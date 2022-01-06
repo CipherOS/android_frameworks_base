0c8de05f1f9e0aee99c1b956736df34b7a415e6a
Pranav Vashi <neobuddy89@gmail.com>
2022-01-06 22:44:10 +0530

    Fixes and clean up for QS brightness slider customizations

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1270,7 +1270,6 @@ public class StatusBar extends SystemUI implements
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
-                    mContext,
                     mNotificationShadeWindowView,
                     mNotificationPanelViewController,
                     mNotificationShadeDepthControllerLazy.get(),
