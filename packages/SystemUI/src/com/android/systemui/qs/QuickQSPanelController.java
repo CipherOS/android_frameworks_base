a7a62681e70df2a97ff8467beeb39d61353aff06
IacobIonut01 <paulionut2003@gmail.com>
2022-04-01 17:38:27 +0000

    SystemUI: Redesign Quick settings
    Author: IacobIonut01 <paulionut2003@gmail.com>
    Date:   Fri Apr 1 17:38:27 2022 +0000
    
        SystemUI: Redesign Quick settings
    
        - Brings up the A11 style of tiles
          + Added morph status of tiles (Circle - Active | Rounded Rect - Inactive/Disabled)
          + Kept compatibility with the secondaryLabel from A12
        - Initial support for columns/rows control in QQS, QS and Customizer
        - Removed color differencce between QSPanel and Notification Drawer
        - Aligned Notification Footer buttons with the notifications (History | Clear All)
        - Added transparency for blur support
        [Temporary removed] - Keep showing notifications when the QSPanel is fully expanded
    
        Bugs / W.I.P.....
        [Temporary removed] - After exiting the QS Customizer, the Notification Drawer's scrim color goes to TRANSPARENT until the user scroll slightly
    
        [Linux4: Remove changes made to QuickStatusBarHeader.java]
        [Linux4: Adapt for T]
    
        Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    
    Author: Tim Zimmermann <tim@linux4.de>
    Date:   Thu Jun 30 17:43:05 2022 +0200
    
        SystemUI: Use colorSurfaceHeader for QS background again
    
        * We want it to be pure black in dark theme, like on R
    
        Change-Id: I43cf9a1999601d1ce967fa79862f7153b5a24ff8
    
    Author: SamarV-121 <samarvispute121@pm.me>
    Date:   Thu Sep 29 03:34:10 2022 +0000
    
        base: Bring carrier info back on qs status bar header
    
        Change-Id: I80c18bb655bd71e01859cc72063c1aafd8fb2908
    
    Author: SamarV-121 <samarvispute121@pm.me>
    Date:   Sat Dec 31 09:08:19 2022 +0000
    
        SystemUI: Increase qqs top padding
    
         * needed after new combined header in qpr2
    
        Change-Id: I0782b5f1665d32566e20bd1d2cef4dbed7f7ce70
    
    [@techyminati - CipherOS]
    - DO NOT MAKE QS ICONS Rounded Rectangular in InActive Mode
    - Recolor QS Tiles text
    
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: techyminati <techyminati@outlook.com>
    Change-Id: I9630d414cd6db3b896dcc6f0591b1cd728012f36

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
@@ -90,6 +90,7 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
     @Override
     protected void onViewAttached() {
         super.onViewAttached();
+        mView.updateColumns();
     }
 
     @Override
@@ -99,6 +100,7 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
 
     private void setMaxTiles(int parseNumTiles) {
         mView.setMaxTiles(parseNumTiles);
+        mView.updateColumns();
         setTiles();
     }
 
@@ -109,6 +111,7 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
             setMaxTiles(newMaxTiles);
         }
         updateMediaExpansion();
+        mView.updateColumns();
     }
 
     @Override
