606dc2fba38748a5e1d5ce5da39ef401880d42b2
jhenrique09 <jhsv09@gmail.com>
2023-11-14 06:59:55 +0100

    [SQUASH] fwb: Switch Back to PixelPropUtils
    * Imported from https://github.com/PixelExperience-Staging/frameworks_base/commit/edcf88e7fd8a3967ae42d3f9c393f740f29d887b
    
    Change-Id: I2b3d44a913f227854cba688e1b88acb95156d9df

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -278,8 +278,7 @@ import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
-
-import com.android.internal.util.PropImitationHooks;
+import com.android.internal.util.custom.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1956,7 +1955,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("getFocusedRootTaskInfo()");
         }
         final long ident = Binder.clearCallingIdentity();
@@ -3068,7 +3067,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("registerTaskStackListener()");
         }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
